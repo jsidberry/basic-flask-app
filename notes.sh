f874e533c5e13e3a676b1504973f8a8bc7eebc18
Juan Sidberry <jsidberry@gmail.com>
2022-05-04 06:40:20 -0400

    fix typos and clear comments

--- notes.sh
+++ notes.sh
@@ -1,12 +1,10 @@
 # commands and notes to setup and use 
-# Flask Virtual Environemnts for Development
+# Flask Virtual Environments for Development
 
-# pyenv exec python3 -m venv .venv
 python3 -m venv .venv
 source .venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
-# pip install Flask 
 
 export FLASK_APP=app.py
 export FLASK_ENV=development
